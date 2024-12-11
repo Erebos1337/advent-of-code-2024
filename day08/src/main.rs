b7416c6f6fca97309ed2ef56f82f37c455515b0a
Thomas Milde <thomas_milde@hotmail.de>
2024-12-11 02:36:49 +0100

    day09 part1 solution

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use utils::inputs::read_lines;
 
-fn calc_new_pos(x: i32, y:i32, dx:i32, dy:i32, width: i32) -> usize {
+fn calc_new_pos(x: i32, y: i32, dx: i32, dy: i32, width: i32) -> usize {
     let new_x = x + dx;
     let new_y = y + dy;
     if new_x >= 0 && new_y >= 0 && new_x < width && new_y < width {
@@ -13,7 +13,7 @@ fn calc_new_pos(x: i32, y:i32, dx:i32, dy:i32, width: i32) -> usize {
 }
 
 fn main() {
-    let lines: Vec<String> = read_lines("./input.txt").unwrap().flatten().collect();
+    let lines: Vec<String> = read_lines("./day08/input.txt").unwrap().flatten().collect();
 
     let width = lines.len();
     let mut char_array: Box<[char]> = vec!['.'; width * width].into_boxed_slice();
@@ -37,28 +37,28 @@ fn main() {
                 let (x2, y2) = positions[j];
                 let dx = x2 as i32 - x1 as i32;
                 let dy = y2 as i32 - y1 as i32;
-                let mut mul:i32 = 1;
+                let mut mul: i32 = 1;
                 let mut new_pos = calc_new_pos(x1 as i32, y1 as i32, dx, dy, w);
                 while new_pos != usize::MAX {
                     antinodes[new_pos] = true;
                     mul += 1;
-                    new_pos = calc_new_pos(x1 as i32, y1 as i32, mul*dx, mul*dy, w);
+                    new_pos = calc_new_pos(x1 as i32, y1 as i32, mul * dx, mul * dy, w);
                 }
                 mul = -1;
-                new_pos = calc_new_pos(x2 as i32, y2 as i32, mul*dx, mul*dy, w);
+                new_pos = calc_new_pos(x2 as i32, y2 as i32, mul * dx, mul * dy, w);
                 while new_pos != usize::MAX {
                     antinodes[new_pos] = true;
                     mul -= 1;
-                    new_pos = calc_new_pos(x2 as i32, y2 as i32, mul*dx, mul*dy, w);
+                    new_pos = calc_new_pos(x2 as i32, y2 as i32, mul * dx, mul * dy, w);
                 }
             }
         }
     }
-    let mut solution:u32 = 0;
+    let mut solution: u32 = 0;
     for antinode in antinodes {
         if antinode {
-            solution+=1;
+            solution += 1;
         }
     }
-    println!("{}", solution);
+    println!("{}", solution); // 1077
 }
--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,3 +1,55 @@
+use utils::inputs::read_lines;
+
+fn is_file(idx: usize) -> bool {
+    idx % 2 == 0
+}
+
+fn file_index(idx: usize) -> u64 {
+    (idx / 2) as u64
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lines: Vec<String> = read_lines("./day09/input.txt").unwrap().flatten().collect();
+    let mut input = lines[0]
+        .chars()
+        .flat_map(|c| c.to_digit(10))
+        .collect::<Vec<u32>>()
+        .into_boxed_slice();
+    println!("input length: {}", input.len());
+
+    let mut solution: u64 = 0;
+
+    let mut lptr: usize = 0;
+    let mut rptr: usize = input.len() - 1;
+    let mut memory_pos: u64 = 0;
+
+    while lptr <= rptr {
+        let length = input[lptr];
+        if is_file(lptr) {
+            if length == 0 {
+                lptr += 1;
+            } else {
+                solution += memory_pos * file_index(lptr);
+                memory_pos += 1;
+                input[lptr] -= 1;
+            }
+        } else {
+            if length == 0 {
+                lptr += 1;
+            } else {
+                if input[rptr] == 0 {
+                    rptr -= 2;
+                } else {
+                    solution += memory_pos * file_index(rptr);
+                    memory_pos += 1;
+                    input[lptr] -= 1;
+                    input[rptr] -= 1;
+                }
+            }
+        }
+    }
+
+    println!("day  9");
+    println!("  - part 1: {}", solution); // 6356833654075
+    // println!("  - part 2: {}", solution2);
 }
