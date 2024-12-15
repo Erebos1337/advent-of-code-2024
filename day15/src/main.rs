634eee524b7bda32c8c86be0e52b4d84d78415e8
Thomas Milde <thomas_milde@hotmail.de>
2024-12-15 17:18:29 +0100

    day15 part2 solution

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -2,7 +2,7 @@ use std::str::Lines;
 
 fn read_map(lines: &mut Lines) -> (Box<[Box<[char]>]>, usize, usize) {
     let mut grid_lines: Vec<Box<[char]>> = Vec::new();
-    while let Some(line) = lines.next() {
+    for line in lines.by_ref() {
         if line.is_empty() {
             break;
         }
@@ -12,18 +12,18 @@ fn read_map(lines: &mut Lines) -> (Box<[Box<[char]>]>, usize, usize) {
     let width = grid[0].len();
     let height = grid.len();
 
-    return (grid, width, height);
+    (grid, width, height)
 }
 
 fn read_movements(lines: &mut Lines) -> Box<[char]> {
     let mut movements_vec: Vec<char> = Vec::new();
-    while let Some(line) = lines.next() {
+    for line in lines.by_ref() {
         movements_vec.extend(line.trim().chars());
     }
-    return movements_vec.into_boxed_slice();
+    movements_vec.into_boxed_slice()
 }
 
-fn find_robot(grid: &mut Box<[Box<[char]>]>, width: usize, height: usize) -> (usize, usize) {
+fn find_robot(grid: &mut [Box<[char]>], width: usize, height: usize) -> (usize, usize) {
     for y in 1..height - 1 {
         for x in 1..width - 1 {
             if grid[y][x] == '@' {
@@ -35,7 +35,7 @@ fn find_robot(grid: &mut Box<[Box<[char]>]>, width: usize, height: usize) -> (us
     panic!("Robot not found");
 }
 
-fn move_right(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) {
+fn move_right(grid: &mut [Box<[char]>], x: usize, y: usize) -> (usize, usize) {
     let new_x = x + 1;
     let mut should_move = false;
 
@@ -55,13 +55,13 @@ fn move_right(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usiz
     }
 
     if should_move {
-        return (new_x, y);
+        (new_x, y)
     } else {
-        return (x, y);
+        (x, y)
     }
 }
 
-fn move_left(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) {
+fn move_left(grid: &mut [Box<[char]>], x: usize, y: usize) -> (usize, usize) {
     let new_x = x - 1;
     let mut should_move = false;
 
@@ -81,13 +81,13 @@ fn move_left(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize
     }
 
     if should_move {
-        return (new_x, y);
+        (new_x, y)
     } else {
-        return (x, y);
+        (x, y)
     }
 }
 
-fn move_down(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) {
+fn move_down(grid: &mut [Box<[char]>], x: usize, y: usize) -> (usize, usize) {
     let new_y: usize = y + 1;
     let mut should_move = false;
 
@@ -107,13 +107,13 @@ fn move_down(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize
     }
 
     if should_move {
-        return (x, new_y);
+        (x, new_y)
     } else {
-        return (x, y);
+        (x, y)
     }
 }
 
-fn move_up(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) {
+fn move_up(grid: &mut [Box<[char]>], x: usize, y: usize) -> (usize, usize) {
     let new_y: usize = y - 1;
     let mut should_move = false;
 
@@ -133,19 +133,14 @@ fn move_up(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) 
     }
 
     if should_move {
-        return (x, new_y);
+        (x, new_y)
     } else {
-        return (x, y);
+        (x, y)
     }
 }
 
-fn solve1(
-    original_grid: &Box<[Box<[char]>]>,
-    movements: &Box<[char]>,
-    width: usize,
-    height: usize,
-) -> usize {
-    let mut grid = original_grid.clone();
+fn solve1(original_grid: &[Box<[char]>], movements: &[char], width: usize, height: usize) -> usize {
+    let mut grid = original_grid.to_vec().clone().into_boxed_slice();
     let (mut x, mut y) = find_robot(&mut grid, width, height);
 
     for movement in movements {
@@ -166,16 +161,306 @@ fn solve1(
         }
     }
 
-    let mut solution1 = 0;
+    let mut solution = 0;
     for y in 1..height - 1 {
         for x in 1..width - 1 {
             if grid[y][x] == 'O' {
-                solution1 += 100 * y + x;
+                solution += 100 * y + x;
             }
         }
     }
 
-    return solution1;
+    solution
+}
+
+fn move_right_wide(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) {
+    let new_x = x + 1;
+    let mut should_move = false;
+
+    if grid[y][new_x] == '.' {
+        should_move = true;
+    } else if grid[y][new_x] == '[' {
+        let mut free_x = new_x + 2;
+        while grid[y][free_x] == '[' {
+            free_x += 2;
+        }
+        if grid[y][free_x] == '.' {
+            grid[y][new_x] = '.';
+            for i in (new_x + 1..free_x).step_by(2) {
+                grid[y][i] = '[';
+                grid[y][i + 1] = ']';
+            }
+
+            should_move = true;
+        }
+    }
+
+    if should_move {
+        (new_x, y)
+    } else {
+        (x, y)
+    }
+}
+
+fn move_left_wide(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) {
+    let new_x = x - 1;
+    let mut should_move = false;
+
+    if grid[y][new_x] == '.' {
+        should_move = true;
+    } else if grid[y][new_x] == ']' {
+        let mut free_x = new_x - 2;
+        while grid[y][free_x] == ']' {
+            free_x -= 2;
+        }
+        if grid[y][free_x] == '.' {
+            grid[y][new_x] = '.';
+            for i in (free_x..new_x).step_by(2) {
+                grid[y][i] = '[';
+                grid[y][i + 1] = ']';
+            }
+
+            should_move = true;
+        }
+    }
+
+    if should_move {
+        (new_x, y)
+    } else {
+        (x, y)
+    }
+}
+
+fn move_down_wide(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) {
+    let new_y: usize = y + 1;
+    let mut should_move = false;
+
+    fn can_move_down(grid: &[Box<[char]>], x: usize, y: usize) -> bool {
+        match grid[y][x] {
+            '.' => true,
+            '[' => can_move_down(grid, x, y + 1) && can_move_down(grid, x + 1, y + 1),
+            ']' => can_move_down(grid, x, y + 1) && can_move_down(grid, x - 1, y + 1),
+            _ => false,
+        }
+    }
+
+    fn collect_updates(
+        grid: &mut Box<[Box<[char]>]>,
+        resets: &mut Vec<(usize, usize)>,
+        updates: &mut Vec<(usize, usize, char)>,
+        x: usize,
+        y: usize,
+    ) {
+        let new_y = y + 1;
+        let curr_char = grid[y][x];
+        resets.push((x, y));
+        updates.push((x, new_y, curr_char));
+
+        let next_char = grid[new_y][x];
+
+        if next_char != '.' {
+            collect_updates(grid, resets, updates, x, new_y);
+        }
+
+        if curr_char == '[' && next_char == ']' {
+            collect_updates(grid, resets, updates, x - 1, new_y);
+        } else if curr_char == ']' && next_char == '[' {
+            collect_updates(grid, resets, updates, x + 1, new_y);
+        }
+    }
+
+    fn push_down(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) {
+        let new_y = y + 1;
+
+        let mut resets: Vec<(usize, usize)> = Vec::new();
+        let mut updates: Vec<(usize, usize, char)> = Vec::new();
+
+        collect_updates(grid, &mut resets, &mut updates, x, new_y);
+
+        let above_char = grid[new_y][x];
+        if above_char == '[' {
+            collect_updates(grid, &mut resets, &mut updates, x + 1, new_y);
+        } else if above_char == ']' {
+            collect_updates(grid, &mut resets, &mut updates, x - 1, new_y);
+        }
+
+        for (x, y) in resets {
+            grid[y][x] = '.';
+        }
+        for (x, y, c) in updates {
+            grid[y][x] = c;
+        }
+    }
+
+    if grid[new_y][x] == '.' {
+        should_move = true;
+    } else if grid[new_y][x] == '[' {
+        if can_move_down(grid, x, new_y) && can_move_down(grid, x + 1, new_y) {
+            push_down(grid, x, y);
+
+            should_move = true;
+        }
+    } else if grid[new_y][x] == ']' {
+        if can_move_down(grid, x, new_y) && can_move_down(grid, x - 1, new_y) {
+            push_down(grid, x, y);
+
+            should_move = true;
+        }
+    }
+
+    if should_move {
+        (x, new_y)
+    } else {
+        (x, y)
+    }
+}
+
+fn move_up_wide(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) -> (usize, usize) {
+    let new_y: usize = y - 1;
+    let mut should_move = false;
+
+    fn can_move_up(grid: &[Box<[char]>], x: usize, y: usize) -> bool {
+        match grid[y][x] {
+            '.' => true,
+            '[' => can_move_up(grid, x, y - 1) && can_move_up(grid, x + 1, y - 1),
+            ']' => can_move_up(grid, x, y - 1) && can_move_up(grid, x - 1, y - 1),
+            _ => false,
+        }
+    }
+
+    fn collect_updates(
+        grid: &mut Box<[Box<[char]>]>,
+        resets: &mut Vec<(usize, usize)>,
+        updates: &mut Vec<(usize, usize, char)>,
+        x: usize,
+        y: usize,
+    ) {
+        let new_y = y - 1;
+        let curr_char = grid[y][x];
+        resets.push((x, y));
+        updates.push((x, new_y, curr_char));
+
+        let next_char = grid[new_y][x];
+
+        if next_char != '.' {
+            collect_updates(grid, resets, updates, x, new_y);
+        }
+
+        if curr_char == '[' && next_char == ']' {
+            collect_updates(grid, resets, updates, x - 1, new_y);
+        } else if curr_char == ']' && next_char == '[' {
+            collect_updates(grid, resets, updates, x + 1, new_y);
+        }
+    }
+
+    fn push_up(grid: &mut Box<[Box<[char]>]>, x: usize, y: usize) {
+        let new_y = y - 1;
+
+        let mut resets: Vec<(usize, usize)> = Vec::new();
+        let mut updates: Vec<(usize, usize, char)> = Vec::new();
+
+        collect_updates(grid, &mut resets, &mut updates, x, new_y);
+
+        let above_char = grid[new_y][x];
+        if above_char == '[' {
+            collect_updates(grid, &mut resets, &mut updates, x + 1, new_y);
+        } else if above_char == ']' {
+            collect_updates(grid, &mut resets, &mut updates, x - 1, new_y);
+        }
+
+        for (x, y) in resets {
+            grid[y][x] = '.';
+        }
+        for (x, y, c) in updates {
+            grid[y][x] = c;
+        }
+    }
+
+    if grid[new_y][x] == '.' {
+        should_move = true;
+    } else if grid[new_y][x] == '[' {
+        if can_move_up(grid, x, new_y) && can_move_up(grid, x + 1, new_y) {
+            push_up(grid, x, y);
+
+            should_move = true;
+        }
+    } else if grid[new_y][x] == ']' {
+        if can_move_up(grid, x, new_y) && can_move_up(grid, x - 1, new_y) {
+            push_up(grid, x, y);
+
+            should_move = true;
+        }
+    }
+
+    if should_move {
+        (x, new_y)
+    } else {
+        (x, y)
+    }
+}
+
+fn solve2(
+    original_grid: &[Box<[char]>],
+    movements: &[char],
+    original_width: usize,
+    height: usize,
+) -> usize {
+    let width = original_width * 2;
+    let mut grid = vec![vec!['.'; width].into_boxed_slice(); height].into_boxed_slice();
+
+    let (mut rx, mut ry) = (0, 0);
+    {
+        for y in 0..height {
+            for x in 0..original_width {
+                let c = original_grid[y][x];
+                match c {
+                    '#' => {
+                        grid[y][2 * x] = '#';
+                        grid[y][2 * x + 1] = '#';
+                    }
+                    'O' => {
+                        grid[y][2 * x] = '[';
+                        grid[y][2 * x + 1] = ']';
+                    }
+                    '@' => {
+                        (rx, ry) = (2 * x, y);
+                        grid[y][2 * x] = '.';
+                        grid[y][2 * x + 1] = '.';
+                    }
+                    _ => continue,
+                }
+            }
+        }
+    }
+
+    for movement in movements {
+        match movement {
+            '>' => {
+                (rx, ry) = move_right_wide(&mut grid, rx, ry);
+            }
+            '<' => {
+                (rx, ry) = move_left_wide(&mut grid, rx, ry);
+            }
+            'v' => {
+                (rx, ry) = move_down_wide(&mut grid, rx, ry);
+            }
+            '^' => {
+                (rx, ry) = move_up_wide(&mut grid, rx, ry);
+            }
+            _ => panic!("Invalid movement"),
+        }
+    }
+
+    let mut solution = 0;
+    for y in 1..height - 1 {
+        for x in 1..width - 1 {
+            if grid[y][x] == '[' {
+                solution += 100 * y + x;
+            }
+        }
+    }
+
+    solution
 }
 
 fn main() {
@@ -186,4 +471,5 @@ fn main() {
 
     println!("day 15");
     println!("  - part 1: {}", solve1(&grid, &movements, width, height)); // 1527563
+    println!("  - part 2: {}", solve2(&grid, &movements, width, height)); // 1521635
 }
--- utils/src/grid.rs
+++ utils/src/grid.rs
@@ -6,3 +6,16 @@ pub fn print_char_grid(grid: &Box<[Box<[char]>]>) {
         println!();
     }
 }
+
+pub fn print_char_grid_mark(grid: &Box<[Box<[char]>]>, (mx, my): (usize, usize)) {
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if x == mx && y == my {
+                print!("@");
+            } else {
+                print!("{}", &grid[y][x]);
+            }
+        }
+        println!();
+    }
+}
