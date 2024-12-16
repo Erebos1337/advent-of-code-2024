9b82317010233ae4c7506eec37731fb12b7b2186
Thomas Milde <thomas_milde@hotmail.de>
2024-12-16 11:20:00 +0100

    clippy cleanup

--- utils/src/conversion.rs
+++ utils/src/conversion.rs
@@ -1,3 +1,3 @@
 pub fn to_int(string: &str) -> u32 {
-    return string.parse().unwrap();
+    string.parse().unwrap()
 }
--- utils/src/grid.rs
+++ utils/src/grid.rs
@@ -1,4 +1,4 @@
-pub fn print_char_grid(grid: &Box<[Box<[char]>]>) {
+pub fn print_char_grid(grid: &[Box<[char]>]) {
     for y in 0..grid.len() {
         for x in 0..grid[y].len() {
             print!("{}", &grid[y][x]);
@@ -7,7 +7,7 @@ pub fn print_char_grid(grid: &Box<[Box<[char]>]>) {
     }
 }
 
-pub fn print_char_grid_mark(grid: &Box<[Box<[char]>]>, (mx, my): (usize, usize)) {
+pub fn print_char_grid_mark(grid: &[Box<[char]>], (mx, my): (usize, usize)) {
     for y in 0..grid.len() {
         for x in 0..grid[y].len() {
             if x == mx && y == my {
